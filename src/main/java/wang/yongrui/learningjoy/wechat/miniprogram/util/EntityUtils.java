c19e9c7c180d2aa581b8c6ee05a16857fbe4ca82
Wang Yongrui <w_y_r@live.cn>
2017-10-14 12:16:45 +0800

    Add method to get heritage info

--- src/main/java/wang/yongrui/learningjoy/wechat/miniprogram/util/EntityUtils.java
+++ src/main/java/wang/yongrui/learningjoy/wechat/miniprogram/util/EntityUtils.java
@@ -3,6 +3,7 @@
  */
 package wang.yongrui.learningjoy.wechat.miniprogram.util;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -37,4 +38,31 @@ public class EntityUtils {
 
 		return objSet;
 	}
+
+	/**
+	 * @param entitySet
+	 * @param entityClass
+	 * @param objectClass
+	 * @return
+	 */
+	public static <E, O> Set<O> getObjectSetFromEntitySetWithHeritage(Set<E> entitySet, Class<E> entityClass,
+			Class<O> objectClass) {
+		Set<O> objSet = null;
+		if (CollectionUtils.isNotEmpty(entitySet)) {
+			objSet = new LinkedHashSet<>();
+			for (E entity : entitySet) {
+				try {
+					O obj = objectClass.getConstructor(entityClass).newInstance(entity);
+					BeanUtils.copyProperties(entity, obj);
+					objSet.add(obj);
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return objSet;
+	}
+
 }
